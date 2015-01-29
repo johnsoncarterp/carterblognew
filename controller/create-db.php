d35d73dad9ebf9db010eb341f6a2e8310d9e29f4
carter johnson <carter32lax@yahoo.com>
2015-01-29 14:01:42 -0800

    on time 5:46 of video 12

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,3 +1,20 @@
 <?php
-require_once("../model/database.php");
+require_once(__DIR__ . "/../model/database.php");
 
+$connection = new mysqli($host, $username, $password);
+
+if($connection->connect_error) {
+    die("Error: " . $connection->connect_error);
+}
+
+$exsists = $connection->select_db($database);
+
+if (!$exsists) {
+    $query = $connection->query("CREATE DATABASE $database");
+    
+    if($query) {
+        echo "Successfully created database: " . $database;
+    }
+}
+
+$connection->close();
