98fbab5e42e7e8bbbcb7cb8510ade119d978f6a7
carter johnson <carter32lax@yahoo.com>
2015-01-30 14:00:57 -0800

    on video 17

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,20 +1,37 @@
 <?php
+
 require_once(__DIR__ . "/../model/database.php");
 
 $connection = new mysqli($host, $username, $password);
 
-if($connection->connect_error) {
-    die("Error: " . $connection->connect_error);
+if ($connection->connect_error) {
+    die("<p>Error: " . $connection->connect_error. "</p>");
 }
 
 $exsists = $connection->select_db($database);
 
 if (!$exsists) {
     $query = $connection->query("CREATE DATABASE $database");
-    
-    if($query) {
-        echo "Successfully created database: " . $database;
+
+    if ($query) {
+        echo "<p>Successfully created database: " . $database . "</p>";
     }
+} else {
+    echo "<p>Database already exsist</p>";
+}
+
+$query = $connection->query("CREATE TABLE posts ("
+        . "id int (11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar (255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query) {
+    echo "<p>Succesfuly created table: posts</p>";
+}
+
+else {
+    echo "<p>$connection->error</p>";
 }
 
 $connection->close();
