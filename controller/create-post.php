df5c1361391256447bb8ba19e48194b0fdc26183
carter johnson <carter32lax@yahoo.com>
2015-02-05 14:01:28 -0800

    on time 9:30  of video 24

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,3 +1,20 @@
 <?php
+require_once (__DIR__ . "/../model/database.php");
+
+$connection = new mysqli($host, $username, $password, $database);
+
+
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-$post; = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+$query = $connection->query("INSERT INTO posts SET title= '$title', post= '$post'");
+
+if($query) {
+    echo "<p>Succssesfully Inserted Posts: $title</p>";
+}
+
+else {
+    echo "<p>$connection->error</p>";
+}
+
+$connection->close();
