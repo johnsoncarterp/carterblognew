c2d1da241fb8619df634bb780324e6531d22775d
carter johnson <carter32lax@yahoo.com>
2015-02-17 13:56:38 -0800

    on video 39

--- view/register-form.php
+++ view/register-form.php
@@ -1,11 +1,20 @@
 <form>
     <div>
-        
+        <label for="email">Email: </label>
+        <imput type="text" name="email" />
     </div>
+    
     <div>
-        
+       <label for="username">Username: </label>
+     <imput type="text" name="username" /> 
     </div>
+    
     <div>
-        
+        <label for="password">Password: </label>
+     <imput type="password" name="password" />
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
     </div>
 </form>
