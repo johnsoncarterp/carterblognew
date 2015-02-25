f96492271ceed4e458851cbcf2060f715f4c5fc0
carter johnson <carter32lax@yahoo.com>
2015-02-25 14:00:40 -0800

    working on style

--- view/header.php
+++ view/header.php
@@ -8,14 +8,26 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Carter Johnson</title>
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
          <link type="text/css" rel="stylesheet" href="css/custom-style.css">
                  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-    <button><a href="register.php">Register</a></button>
-    <button><a href="index.php">Home</a></button>
-    <button><a href="login.php">Login</a></button>
-    <button><a href="/carterblog/controller/logout-user.php">Logout</a></button>
+        
+    <nav>        
+        <ul class="nav nav-tabs">
+            <li role="presentation" ><a href="index.php">Home</a></li>
+            <li role="presentation" ><a href="register.php">Register</a></li>
+            <li role="presentation" ><a href="login.php">Login</a></li>
+            <li role="presentation" ><a href="/carterblog/controller/logout-user.php">Logout</a></li></button>
+        </ul>
+    </nav>
+    <article>
+                <div class="jumbotron">
+                <h1>Blog</h1>
+            </div>
+    </article>
     
     </head>
     <body>
--- view/navigation.php
+++ view/navigation.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . "/../model/config.php");
 ?>
 <nav>
-    <ul>
+    <ul class="blog">
         <button><li><a href="<?php echo $path . "post.php"?>">POST BLOG!!!</a></li></button>
     </ul>
 </nav>
