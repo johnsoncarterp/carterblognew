7532b97d2f38bd1076b83ca2e3de7c780c17dd0e
carter johnson <carter32lax@yahoo.com>
2015-02-24 21:46:16 -0800

    finished all of the videos

--- view/header.php
+++ view/header.php
@@ -7,11 +7,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Carter Johnson</title>
          <link type="text/css" rel="stylesheet" href="css/custom-style.css">
+                 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     <button><a href="register.php">Register</a></button>
     <button><a href="index.php">Home</a></button>
     <button><a href="login.php">Login</a></button>
+    <button><a href="/carterblog/controller/logout-user.php">Logout</a></button>
+    
     </head>
     <body>
 
